029b70ec99e36399633192b298b23c224dac81c0
amooryjubran <omarsalehjubran@gmail.com>
2021-02-11 20:43:32 -0500

    reducers file

---
+++ src/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import usersReducer from "./users/reducers/users";
+
+export default (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    users: usersReducer,
+  });
